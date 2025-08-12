4252436a22677b11246947f457d4f905efde4011
HGrewal13 <harrysgrewal13@gmail.com>
2025-08-12 00:13:55 -0400

    city info on DOM is updated with search function

--- ui.js
+++ ui.js
@@ -20,11 +20,22 @@ const UI = (function() {
         currentCity = await dataProcessing.searchProcessing(searchTerm);
     }
 
+    const updateDom = function() {
+        const cityName = document.querySelector(".cityName");
+        const cityDesc = document.querySelector(".cityDescription");
+        const cityTemp = document.querySelector(".cityTemp");
+
+        cityName.textContent = currentCity.location;
+        cityDesc.textContent = currentCity.description;
+        cityTemp.textContent = currentCity.temp;
+    }
+
     // Event Listeners
     searchForm.addEventListener("submit", async e => {
         e.preventDefault();
         const searchTerm = searchForm.search.value;
         await setCurrentCity(searchTerm);
+        updateDom();
         console.log(currentCity);
     })
 
